7ce72e16cce0391bc0fc7c4617fdad081637e2a5
Caleb Krauter <42187569+calebkrauter@users.noreply.github.com>
2023-01-07 18:06:02 -0800

    Create mySet

---
+++ assignment-one-question-3/MySet.java
@@ -0,0 +1,55 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Set;
+
+public class MySet {
+// stores a collection of Objects (in an array
+// stores current size of the collection
+
+public boolean isEmpty() {
+    return true;
+}
+public void makeEmpty() {
+
+}
+public int size() {
+    return 0;
+}
+public void insert() {
+
+}
+public void remove() {
+
+}
+public boolean isPresent() {
+    return false;
+}
+public void test() {
+
+}
+
+}
+
+
+
+//    Write a Java class called MySet,
+//    TODO REQUIREMENTS - that stores a collection of Objects (in an array) along
+//        with the current size of the collection.
+//    TODO REQUIREMENTS - An instance of MySet should behave just like a set:
+//        to the caller of the methods, there is no order associated with the items in the
+//        MySet object.
+//    TODO REQUIREMENTS - Furthermore, there should not be two items in the MySet that are
+//        .equals() to each other as a result of any of the MySet methods.
+//
+//    TODO - Provide public methods isEmpty, makeEmpty, size, insert, remove, and
+//        isPresent.
+//    TODO REQUIREMENTS - remove(x) should remove an item from the array that is equal to x (as
+//        defined by equals).
+//    TODO REQUIREMENTS - isPresent(x) returns true if and only if an Object that is equal
+//        to x (as defined by equals) is present in the collection.
+//    TODO - Carefully think about what the
+//        parameters to the above methods are.
+//    TODO - Write a method called test that makes calls to
+//        your methods to demonstrate that they behave correctly. Be sure to test boundary
+//        cases, such as inserting an item when the array is full.
