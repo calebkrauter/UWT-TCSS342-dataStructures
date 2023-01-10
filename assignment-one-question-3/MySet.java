7be45c0c587eb64d54692813d482e1b40b972522
Caleb Krauter <krautercaleb@gmail.com>
2023-01-10 00:28:52 -0800

    Implement remove function

--- assignment-one-question-3/MySet.java
+++ assignment-one-question-3/MySet.java
@@ -1,11 +1,50 @@
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Objects;
+import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Set;
 
 public class MySet {
 // stores a collection of Objects (in an array
 // stores current size of the collection
+
+    private Object[] array;
+    private Object[] newArray;
+    private Object[] newArray2;
+
+    int myCollectionSize = 6;
+    final Integer x = 2;
+    final Integer obj1 = 1;
+    final Integer obj2 = 2;
+    final Integer obj3 = 3;
+    final Integer obj4 = 1;
+    final Integer obj5 = 2;
+    final Integer obj6 = 3;
+
+
+    public MySet() {
+
+
+        array = new Object[myCollectionSize];
+        // Replace array index with counter variable
+        array[0] = obj1;
+        array[1] = obj2;
+        array[2] = obj3;
+        array[3] = obj4;
+        array[4] = obj5;
+        array[5] = obj6;
+
+        System.out.println(Arrays.toString(array));
+
+        remove(x);
+        System.out.println(Arrays.toString(array));
+//        for (int currentObject: array) {
+//            // Replace '==' with '.equals()'
+//            if (currentObject == x) {
+//                array = new int[myCollectionSize - 1];
+//                array[0] = ;
+//            }
+//        }
+
+    }
 
 public boolean isEmpty() {
     return true;
@@ -16,11 +55,39 @@ public void makeEmpty() {
 public int size() {
     return 0;
 }
-public void insert() {
 
+
+public void insert(int theIndex, Object theObject) {
 }
-public void remove() {
+public void remove(Object x) {
 
+    for (int i = 0; i < array.length; i++) {
+        // Replace == with .equals()
+
+            newArray = Arrays.copyOf(array, myCollectionSize - 1);
+            System.out.println("New array" + Arrays.toString(newArray));
+            // Item at end then cut off end.
+            if (array[i].equals(array[array.length - 1]) && array[i].equals(x)) {
+                System.out.println("Removing item x: " + x + " at end. Updated array: " + Arrays.toString(array));
+                array = newArray;
+            } else if(array[i].equals(x)) {
+//                System.out.println(x);
+            // Swap consecutive pairs until bad item is at end and is cut off.
+                for (int j = i + 1; array.length > j; j++) {
+
+                        newArray[i] = array[j];
+
+                        System.out.println("Swap value " + newArray[i] + " at index " + i + " with " + array[j] + " at index " + j);
+
+                    i++;
+                }
+                array = newArray;
+
+
+                System.out.println("Removing item x: " + x + " at end. Updated array: " + Arrays.toString(array));
+            }
+
+    }
 }
 public boolean isPresent() {
     return false;
