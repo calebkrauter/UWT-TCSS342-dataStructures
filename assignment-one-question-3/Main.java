48fdf4d4379bf90503f0edc1a654cd88b333ddfa
Caleb Krauter <krautercaleb@gmail.com>
2023-01-10 01:36:41 -0800

    Function for insertion, update test function

--- assignment-one-question-3/Main.java
+++ assignment-one-question-3/Main.java
@@ -5,24 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
         MySet mySet = new MySet();
-
-
-
-
-//        int theCollectionSize = 0;
-//        int theObjectIndex = 0;
-//        final Integer obj1 = 0;
-//        theCollectionSize++;
-//        final Object obj2 = "String Object";
-//        theCollectionSize++;
-//        final BigDecimal obj3 = BigDecimal.valueOf(10.00);
-//        theCollectionSize++;
-//
-//        MySet mySet = new MySet(theCollectionSize);
-//
-//        mySet.insert(theObjectIndex, obj1);
-//        mySet.insert(theObjectIndex++, obj2);
-//        mySet.insert(theObjectIndex++, obj3);
     }
 
 }
--- assignment-one-question-3/MySet.java
+++ assignment-one-question-3/MySet.java
@@ -8,6 +8,7 @@ public class MySet {
 
     private Object[] array;
     private Object[] newArray;
+    private int myIndexCounter = 0;
     private Object[] newArray2;
 
     int myCollectionSize = 6;
@@ -18,81 +19,83 @@ public class MySet {
     final Integer obj4 = 1;
     final Integer obj5 = 2;
     final Integer obj6 = 3;
-
+    int arraySizeCounter = 0;
 
     public MySet() {
 
+        array = new Object[arraySizeCounter++];
 
-        array = new Object[myCollectionSize];
-        // Replace array index with counter variable
-        array[0] = obj1;
-        array[1] = obj2;
-        array[2] = obj3;
-        array[3] = obj4;
-        array[4] = obj5;
-        array[5] = obj6;
+        // Creates array to collect objects.
 
-        System.out.println(Arrays.toString(array));
-
-        remove(x);
-        System.out.println(Arrays.toString(array));
-//        for (int currentObject: array) {
-//            // Replace '==' with '.equals()'
-//            if (currentObject == x) {
-//                array = new int[myCollectionSize - 1];
-//                array[0] = ;
-//            }
-//        }
-
+        test();
     }
 
-public boolean isEmpty() {
-    return true;
-}
-public void makeEmpty() {
+    public boolean isEmpty() {
+        return true;
+    }
+    public void makeEmpty() {
 
-}
-public int size() {
-    return 0;
-}
+    }
+    public int size() {
+        return 0;
+    }
 
 
-public void insert(int theIndex, Object theObject) {
-}
-public void remove(Object x) {
+    public void insert(Object theObject) {
+        // Replace array index with counter variable.
+        arraySizeCounter = myIndexCounter + 1;
+        array = Arrays.copyOf(array, arraySizeCounter++);
+        array[myIndexCounter] = theObject;
+        myIndexCounter++;
+    }
+    public void remove(Object x) {
+        if (array.length > 2) {
+            for (int i = 0; i < array.length; i++) {
+                // Replace == with .equals()
 
-    for (int i = 0; i < array.length; i++) {
-        // Replace == with .equals()
+                newArray = Arrays.copyOf(array, array.length - 1);
 
-            newArray = Arrays.copyOf(array, myCollectionSize - 1);
-            System.out.println("New array" + Arrays.toString(newArray));
-            // Item at end then cut off end.
-            if (array[i].equals(array[array.length - 1]) && array[i].equals(x)) {
-                System.out.println("Removing item x: " + x + " at end. Updated array: " + Arrays.toString(array));
-                array = newArray;
-            } else if(array[i].equals(x)) {
-//                System.out.println(x);
-            // Swap consecutive pairs until bad item is at end and is cut off.
-                for (int j = i + 1; array.length > j; j++) {
+                // Bad item at end then cut off end.
+                if (array[i].equals(array[array.length - 1]) && array[i].equals(x)) {
 
+                    array = newArray;
+                } else if (array[i].equals(x)) {
+                    // Swap consecutive pairs until bad item is at end and is cut off.
+                    for (int j = i + 1; array.length > j; j++) {
                         newArray[i] = array[j];
-
-                        System.out.println("Swap value " + newArray[i] + " at index " + i + " with " + array[j] + " at index " + j);
-
-                    i++;
+                        i++;
+                    }
+                    array = newArray;
                 }
-                array = newArray;
-
-
-                System.out.println("Removing item x: " + x + " at end. Updated array: " + Arrays.toString(array));
             }
-
+        }
     }
-}
-public boolean isPresent() {
-    return false;
-}
-public void test() {
+
+    public boolean isPresent() {
+        return false;
+    }
+    public void test() {
+
+        // Test insertion of different Objects
+        insert(obj1);
+        insert(obj2);
+        insert(obj3);
+        insert(obj4);
+        insert(obj5);
+        insert(obj6);
+        System.out.println("Full array " + Arrays.toString(array));
+
+        // Test removal duplicate objects.
+        remove(obj2);
+        System.out.println(Arrays.toString(array));
+        remove(obj2);
+        System.out.println(Arrays.toString(array));
+        // Test removal of first object.
+        remove(obj1);
+        System.out.println(Arrays.toString(array));
+        // Test removal of last object.
+        remove(obj3);
+        System.out.println(Arrays.toString(array));
 
 }
 
