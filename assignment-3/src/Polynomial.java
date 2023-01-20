44187aa26674fc6e93a6f468f8ccfc3c42b117a8
Caleb Krauter <42187569+calebkrauter@users.noreply.github.com>
2023-01-19 18:59:35 -0800

    Program runs

---
+++ assignment-3/src/Polynomial.java
@@ -0,0 +1,42 @@
+public class Polynomial {
+    // Fields
+    private LinkedList terms;
+
+
+    // Constructors
+    public Polynomial() {
+
+    }
+
+    // Methods
+    public void insertTerm(int coefficient, int exponent) {
+
+    }
+    public void zeroPolynomial() {
+
+    }
+    public Polynomial negate() {
+        Polynomial value = null;
+        return value;
+    }
+    public Polynomial plus(Polynomial polynomial) {
+        Polynomial value = null;
+        return value;
+    }
+    public Polynomial minus(Polynomial polynomial) {
+        Polynomial value = null;
+        return value;
+    }
+    public Polynomial times(Polynomial polynomial) {
+        Polynomial value = null;
+        return value;
+    } // BONUS method
+
+    public Polynomial derivative() {
+        Polynomial value = null;
+        return value;
+    }
+    public String print() {
+        return "String";
+    }
+}
